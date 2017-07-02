10304b8d661b58764eab0aa6ad2ebd38c3f0ba02
Zhenyu <zhenyu051988@gmail.com>
2017-07-02 00:08:53 -0700

    Create the controller and show the table. Debugging...

--- MiniEmployeeManagementSystem_Final/src/com/ems/Model.java
+++ MiniEmployeeManagementSystem_Final/src/com/ems/Model.java
@@ -1,14 +1,17 @@
-package com.ems;
-
-import javax.swing.table.AbstractTableModel;
-import java.util.Vector;
-
 /**
  * This class is to create the model. It will display the data fetched
  * from database
  * @author Jeffery
  *
  */
+package com.ems;
+
+import javax.swing.table.AbstractTableModel;
+
+import java.sql.ResultSet;
+import java.util.Vector;
+
+
 public class Model extends AbstractTableModel{
 	
 	//To store the data of each row
@@ -18,8 +21,10 @@ public class Model extends AbstractTableModel{
 	
 	public void printTable(String query, String tableName){
 		columnTitle = new Vector<String>();
+		//Declare a controller object to pass the query to database
+		Controller crl = null;
 		//Fill the title column with column name of employee table  
-		if(tableName.equals("Employee")){
+		if(tableName.equals("employee")){
 			columnTitle.add("EmployeeId");
 			columnTitle.add("LastName");
 			columnTitle.add("FirstName");
@@ -27,10 +32,35 @@ public class Model extends AbstractTableModel{
 			columnTitle.add("DepartmentId");
 			columnTitle.add("Position");
 			columnTitle.add("BaseSalary");
-			
+			//Initialize a rowData vector to store data of row
 			rowData = new Vector();
 			
-		}else if(tableName.equals("Person")){
+			//Fill each row with data fetched from database
+			try {
+				crl = new Controller();
+				ResultSet rls = crl.getResult(query);
+				while(rls.next()){
+					Vector row = new Vector();
+					row.add(rls.getString(1));
+					row.add(rls.getString(2));
+					row.add(rls.getString(3));
+					row.add(rls.getString(4));
+					row.add(rls.getString(5));
+					row.add(rls.getString(6));
+					row.add(rls.getString(7));
+					
+					rowData.add(row);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally{
+				crl.closeDB();
+			}
+			
+			
+			
+		}else if(tableName.equals("person")){
 			columnTitle.add("SIN#");
 			columnTitle.add("Gender");
 			columnTitle.add("Age");
@@ -39,33 +69,83 @@ public class Model extends AbstractTableModel{
 
 			
 			rowData = new Vector();
+
+			//Fill each row with data fetched from database
+			try {
+				crl = new Controller();
+				ResultSet rls = crl.getResult(query);
+				while(rls.next()){
+					Vector row = new Vector();
+					row.add(rls.getString(1));
+					row.add(rls.getString(2));
+					row.add(rls.getString(3));
+					row.add(rls.getString(4));
+					row.add(rls.getString(5));
+
+					
+					rowData.add(row);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally{
+				crl.closeDB();
+			}
 			
-		}else if(tableName.equals("Department")){
+		}else if(tableName.equals("department")){
 			columnTitle.add("DepartmentId");
 			columnTitle.add("DepartmentName");
 			columnTitle.add("DepartmentDuty");
 
 			rowData = new Vector();
+			//Fill each row with data fetched from database
+			try {
+				crl = new Controller();
+				ResultSet rls = crl.getResult(query);
+				while(rls.next()){
+					Vector row = new Vector();
+					row.add(rls.getString(1));
+					row.add(rls.getString(2));
+					row.add(rls.getString(3));
+					
+					rowData.add(row);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally{
+				crl.closeDB();
+			}
 			
 		}
 	}
 
 	@Override
+	//Returns the number of columns in the model.
+	//A JTable uses this method to determine
+	//how many columns it should create and display by default.
 	public int getColumnCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.columnTitle.size();
 	}
 
 	@Override
 	public int getRowCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.rowData.size();
 	}
 
 	@Override
-	public Object getValueAt(int arg0, int arg1) {
+	public Object getValueAt(int row, int column) {
 		// TODO Auto-generated method stub
-		return null;
+		return ((Vector)this.rowData.get(row)).get(column);
 	}
+
+	@Override
+	public String getColumnName(int column) {
+		// TODO Auto-generated method stub
+		return (String) this.getColumnName(column);
+	}
+	
 	
 }
--- MiniEmployeeManagementSystem_Final/src/com/ems/View.java
+++ MiniEmployeeManagementSystem_Final/src/com/ems/View.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.table.TableModel;
+
 import java.util.*;
 
 
@@ -41,7 +43,7 @@ public class View implements ActionListener{
 		jbt1.setPreferredSize(new Dimension(100,30));
 		jbt1.setToolTipText("Search Employee");
 		jbt1.addActionListener(this);
-		jbt1.setActionCommand("Search");
+		jbt1.setActionCommand("search");
 		jp1.add(jtf1);
 		jp1.add(jbt1);
 		
@@ -52,43 +54,43 @@ public class View implements ActionListener{
 		jbt2.setPreferredSize(new Dimension(120,30));
 		jbt2.setToolTipText("View All Employee Info");
 		jbt2.addActionListener(this);
-		jbt2.setActionCommand("Employee");
+		jbt2.setActionCommand("employee");
 		
 		jbt3 = new JButton("Department");
 		jbt3.setPreferredSize(new Dimension(120,30));
 		jbt3.setToolTipText("View All Department Info");
 		jbt3.addActionListener(this);
-		jbt3.setActionCommand("Department");
+		jbt3.setActionCommand("department");
 		
 		jbt4 = new JButton("Person");
 		jbt4.setPreferredSize(new Dimension(120,30));
 		jbt4.setToolTipText("View All Person Info");
 		jbt4.addActionListener(this);
-		jbt4.setActionCommand("Person");
+		jbt4.setActionCommand("person");
 		
 		jbt5 = new JButton("Add");
 		jbt5.setPreferredSize(new Dimension(120,30));
 		jbt5.setToolTipText("Add New Employee");
 		jbt5.addActionListener(this);
-		jbt5.setActionCommand("Add");
+		jbt5.setActionCommand("add");
 		
 		jbt6 = new JButton("Delete");
 		jbt6.setPreferredSize(new Dimension(120,30));
 		jbt6.setToolTipText("Delete Existing Employee");
 		jbt6.addActionListener(this);
-		jbt6.setActionCommand("Delete");
+		jbt6.setActionCommand("delete");
 		
 		jbt7 = new JButton("Modify");
 		jbt7.setPreferredSize(new Dimension(120,30));
 		jbt7.setToolTipText("Modify Data of current Employee");
 		jbt7.addActionListener(this);
-		jbt7.setActionCommand("Modify");
+		jbt7.setActionCommand("modify");
 				
 		jbt7 = new JButton("Exit");
 		jbt7.setPreferredSize(new Dimension(120,30));
 		jbt7.setToolTipText("Exit The System");
 		jbt7.addActionListener(this);
-		jbt7.setActionCommand("Exit");
+		jbt7.setActionCommand("exit");
 		
 		jp2.add(jbt2);
 		jp2.add(jbt3);
@@ -123,22 +125,49 @@ public class View implements ActionListener{
 	//Catch the action and pass the SQL query
 	@Override
 	public void actionPerformed(ActionEvent a) {
+		Model mdl = null;
 		// TODO Auto-generated method stub
-		if(a.getActionCommand().equals("Search")){
-			System.out.println("You click the search");
-		}else if(a.getActionCommand().equals("Employee")){
-			System.out.println("You click the employee");
-		}else if(a.getActionCommand().equals("Person")){
-			System.out.println("You click the person");
-		}else if(a.getActionCommand().equals("Department")){
-			System.out.println("You click the department");
-		}else if(a.getActionCommand().equals("Add")){
+		if(a.getActionCommand().equals("search")){
+			//Get the first name from text field
+			String firstName = this.jtf1.getText().trim();
+			//Generate query
+			String query = "select * from employee where firstName = ' " + firstName + " ' " ;
+			//Create model object to send the query
+			mdl = new Model();
+			mdl.printTable(query, "employee");
+			//Update the table
+			jtb1.setModel(mdl);
+		}else if(a.getActionCommand().equals("employee")){
+			//Generate query
+			String query = "select * from employee";
+			//Create model object to send the query
+			mdl = new Model();
+			mdl.printTable(query, "employee");
+			//Update the table
+			jtb1.setModel(mdl);
+		}else if(a.getActionCommand().equals("person")){
+			//Generate query
+			String query = "select * from person" ;
+			//Create model object to send the query
+			mdl = new Model();
+			mdl.printTable(query, "person");
+			//Update the table
+			jtb1.setModel(mdl);
+		}else if(a.getActionCommand().equals("department")){
+			//Generate query
+			String query = "select * from department" ;
+			//Create model object to send the query
+			mdl = new Model();
+			mdl.printTable(query, "department");
+			//Update the table
+			jtb1.setModel(mdl);
+		}else if(a.getActionCommand().equals("add")){
 			System.out.println("You click the add");
-		}else if(a.getActionCommand().equals("Delete")){
+		}else if(a.getActionCommand().equals("delete")){
 			System.out.println("You click the delete");
-		}else if(a.getActionCommand().equals("Modify")){
+		}else if(a.getActionCommand().equals("modify")){
 			System.out.println("You click the modify");
-		}else if(a.getActionCommand().equals("Exit")){
+		}else if(a.getActionCommand().equals("exit")){
 			System.out.println("You click the exit");
 		}
 	}
